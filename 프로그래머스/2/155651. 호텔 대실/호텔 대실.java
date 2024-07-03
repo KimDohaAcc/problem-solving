3da0ec846e69ad902e12922852e6536c45620beb
김도하 <127382101+KimDohaAcc@users.noreply.github.com>
2024-07-04 00:58:28 +0900

    [level 2] Title: 호텔 대실, Time: 25.32 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] book_time) {
+        // 이 익숙한 전개는 그리디...?
+        
+        List<int[]> bookingList = new ArrayList<>();
+        
+        for(int i = 0; i < book_time.length; i ++) {
+            StringBuilder sb = new StringBuilder(book_time[i][0]);
+            int hour = Integer.parseInt(sb.substring(0, 2));
+            int min = Integer.parseInt(sb.substring(3, 5));
+            int enterTime = hour * 60 + min;
+            
+            sb = new StringBuilder(book_time[i][1]);
+            hour = Integer.parseInt(sb.substring(0, 2));
+            min = Integer.parseInt(sb.substring(3, 5));
+            int leaveTime = hour * 60 + min;
+            
+            bookingList.add(new int[] {enterTime, leaveTime});
+        }
+        
+        Collections.sort(bookingList, new Comparator<int[]>() {
+            
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+            
+        List<Integer> roomList = new ArrayList<>();
+        roomList.add(0);
+        
+        for(int[] booking : bookingList) {
+            Collections.sort(roomList);
+            
+            for(int i = 0; i < roomList.size(); i ++) {
+                if(roomList.get(i) <= booking[0]) {
+                    roomList.set(i, booking[1] + 10);
+                    break;
+                } else if(i == roomList.size() - 1) {
+                    roomList.add(booking[1] + 10);
+                    break;
+                }
+            }
+        }
+        
+        return roomList.size();
+    }
+}
