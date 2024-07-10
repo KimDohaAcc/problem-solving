69bb9a69da9d33c043c714dfd4dcf74522eb9bba
김도하 <127382101+KimDohaAcc@users.noreply.github.com>
2024-07-11 00:37:55 +0900

    [level 3] Title: 등산코스 정하기, Time: 754.04 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -5,83 +5,87 @@ class Solution {
     // 다익스트라 쓰는데 거리 말고 intensity 넣으면 될듯?
     static int intensity = 10000001;
     static int summit = 0;
-    
+
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         List<int[]>[] nodeList = new ArrayList [n + 1];
-        
+
         for(int i = 0; i < nodeList.length; i ++) {
             nodeList[i] = new ArrayList<>();
         }
-        
+
         for(int i = 0; i < paths.length; i ++) {
             int node = paths[i][0];
             int other = paths[i][1];
             int dis = paths[i][2];
-            
+
             nodeList[node].add(new int [] {other, dis});
             nodeList[other].add(new int [] {node, dis});
         }
-        
+
         // 게이트 찾기 좋게 list로 바꿈
         List<Integer> gateList = new ArrayList<>();
         for(int i = 0; i < gates.length; i ++) {
             gateList.add(gates[i]);
         }
+
+        // 번호가 낮은 산봉우리부터 탐색
+        Arrays.sort(summits);
         
         // 산봉우리 찾기 좋게 list로 바꿈
         List<Integer> summitList = new ArrayList<>();
         for(int i = 0; i < summits.length; i ++) {
             summitList.add(summits[i]);
         }
-        
+
         // 다익스트라
-        // 번호가 낮은 산봉우리부터 탐색
-        Arrays.sort(summits);
-        for(int i = 0; i < summits.length; i ++) {
-            dijkstra(summits[i], nodeList, gateList, summitList, n);
-        }
-        
+        dijkstra(nodeList, gateList, summitList, n);
+
         // 산봉우리 번호랑 intensity 넣기
         int[] answer = {summit, intensity};
         return answer;
     }
-    
-    static void dijkstra(int s, List<int[]>[] nodeList, List<Integer> gateList, List<Integer> summitList, int n) {
-        boolean[] visited = new boolean [n + 1];
-        Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
-        
+
+    static void dijkstra(List<int[]>[] nodeList, List<Integer> gateList, List<Integer> summitList, int n) {
+        Queue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] == o2[2] ? o1[1] - o2[1] : o1[2] - o2[2];
+            }
+        });
+
         int[] dis = new int [n + 1];
         Arrays.fill(dis, Integer.MAX_VALUE);
-        int order = 1;
-        
-        pq.offer(new int[] {s, 0});
-        visited[s] = true;
-        dis[s] = 0;
-        
+
+        for (Integer summit : summitList) {
+            pq.offer(new int[] {summit, 0, summit});
+            dis[summit] = 0;
+        }
+
         while(!pq.isEmpty()) {
             int[] arr = pq.poll();
             int now = arr[0];
-            
+            int s = arr[2];
+
             for(int i = 0; i < nodeList[now].size(); i ++) {
                 int[] node = nodeList[now].get(i);
                 int next = node[0];
                 int nextD = node[1];
                 int nowInt = Math.max(dis[now], nextD);
-                
+
                 if(summitList.contains(next) || dis[next] < nowInt || nowInt >= intensity) {
                     continue;
                 }
-                
+
                 if(dis[next] > nowInt) {
                     dis[next] = nowInt;
-                    pq.offer(new int[] {next, nextD});
+                    pq.offer(new int[] {next, nextD, s});
                 }
 
                 if(gateList.contains(next) && dis[next] < intensity) {
                     intensity = dis[next];
                     summit = s;
-                 }
-                
+                }
+
             }
         }
     }
