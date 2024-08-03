ab19b76b08763d9557d5337ec161bd1fb8a9d86f
김도하 <127382101+KimDohaAcc@users.noreply.github.com>
2024-08-03 22:37:04 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 50.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,36 +1,43 @@
+# 벽이 있으면 건설 불가 1
+# 이동은 직선 도로 100
+# 직각으로 만나면 코너 500
+# 최소 비용 구하기
+
 import sys
-import heapq
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+min_cost = sys.maxsize
 
-def dijkstra(board, n):
-    hq = list()
-    dis = [[[sys.maxsize] * 4 for _ in range(n)] for _ in range(n)]
-    heapq.heappush(hq, (0, 0, 0, 1))
-    heapq.heappush(hq, (0, 0, 0, 2))
+def dfs(board, n, visited, x, y, cost, direction):
+    global min_cost, min_dis
+    if min_cost <= cost:
+        return
     
-    dis[0][0][1] = 0
-    dis[0][0][2] = 0
+    if x == n-1 and y == n-1:
+        min_cost = min(min_cost, cost)
+        return
     
-    while hq:
-        cost, x, y, d = heapq.heappop(hq)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
         
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= n or ny >= n or board[nx][ny] == 1:
+            continue
             
-            if nx < 0 or ny < 0 or nx >= n or ny >= n or board[nx][ny] == 1:
-                continue
-                
-            n_cost = cost + 100 + (500 if (d + i) % 2 == 1 else 0)
-            
-            # d는 들어온 방향
-            if dis[nx][ny][d] >= n_cost:
-                dis[nx][ny][d] = n_cost
-                heapq.heappush(hq, (n_cost, nx, ny, i))
-                
-    return min(dis[n-1][n-1])
+        if (nx, ny) in visited:
+            continue
+                 
+        n_cost = cost + 100
+        if (direction + i) % 2 == 1:
+            n_cost += 500
         
+        visited.append((nx, ny))
+        dfs(board, n, visited, nx, ny, n_cost, i)
+        visited.remove((nx, ny))
+    
+
 def solution(board):
-    return dijkstra(board, len(board))
+    dfs(board, len(board), [], 0, 0, 0, 2)
+    dfs(board, len(board), [], 0, 0, 0, 1)
+    return min_cost
