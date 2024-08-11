f3f5b598487a3483d2fec1c33e522cf0dc4c1d42
김도하 <127382101+KimDohaAcc@users.noreply.github.com>
2024-08-11 12:13:53 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/3/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [level 3] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 02일 02:32:34
+2024년 08월 11일 12:13:50
 
 ### 문제 설명
 
--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,36 +1,62 @@
 from collections import deque
 def solution(n, m, x, y, r, c, k):
-    ver = abs(x - r)
-    hor = abs(y - c)
-    dis = ver + hor
-    remain = k - dis
-
-    if dis > k or remain % 2 != 0:
+    min_dis = abs(x - r) + abs(y - c)
+    remain = k - min_dis
+    res = ""
+    
+    if remain < 0 or remain % 2 == 1:
         return "impossible"
-
-    moves = ""
-
-    d_space = max(0, r - x)
-    l_space = max(0, y - c)
-    r_space = max(0, c - y)
-    u_space = max(0, x - r)
-
-    d_move = remain // 2 if remain // 2 < (n - x - d_space) else (n - x - d_space)
-    moves += "d" * (d_move + d_space)
-    remain -= d_move * 2
     
-    l_move = remain // 2 if remain // 2 < (y - l_space - 1) else (y - l_space - 1)
-    moves += "l" * (l_move + l_space)
-    remain -= l_move * 2
+    l_move, r_move, u_move, d_move = 0, 0, 0, 0
     
-    # r 다음에 l이 와야 사전 순 더 빠름 제발 ㅠㅠㅠ
-    if m >= 2:
-        moves += "rl" * (remain // 2)
-    moves += "r" * (r_space + l_move)
-    moves += "u" * (u_space + d_move)
+    if x > r:
+        u_move += x - r
+    else:
+        d_move += r - x
+        
+    if y > c:
+        l_move += y - c
+    else:
+        r_move += c - y
+        
+    print("L", l_move)
+    print("R", r_move)
+    print("D", d_move)
+    print("U", u_move)
+    print("remain : ", remain)
+    print("n - (x + d_move) : ", n - (x + d_move))
     
-    print(d_move)
-    print(l_move)
+    if remain > 0 and n - (x + d_move) > 0:
+        d_move_second = remain // 2 if remain // 2 < n - (x + d_move) else n - (x + d_move)
+        print("second_d ", d_move_second)
+        d_move += d_move_second
+        u_move += d_move_second
+        
+        remain -= d_move_second * 2
 
+    res += "d" * d_move
+        
+    print("y - l_move : ", y - l_move)
+    
+    if remain > 0 and y - l_move > 1:
+        l_move_second = remain // 2 if remain // 2 < y - l_move - 1 else y - l_move - 1
+        
+        l_move += l_move_second
+        r_move += l_move_second
+        
+        remain -= l_move_second * 2
+    
+    res += "l" * l_move
 
-    return moves
+    if remain > 0:
+        rl_move = remain // 2
+        res += "rl" * rl_move
+        
+    res += "r" * r_move
+    res += "u" * u_move
+    
+    print(res)
+    
+    return res
+    
+    
